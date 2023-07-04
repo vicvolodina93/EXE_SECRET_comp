1501a11e13bad73873635504f8a13cbaf7552c10
JSalter90 <jamessalter90@gmail.com>
2023-07-04 13:07:28 +0100

    tidying comments

--- data_comp/load_runs.R
+++ data_comp/load_runs.R
@@ -1,5 +1,5 @@
-# pending: runs 1-250
 # Combining all runs into single data frames
+# Flows 1-7
 flow1_all <- data.frame(read.csv('runs_1_250/flow1_1_250.csv'),
                         read.csv('runs_251_500/flow1_251_500.csv'),
                         read.csv('runs_501_750/flow1_501_750.csv'),
@@ -42,7 +42,7 @@ flow7_all <- data.frame(read.csv('runs_1_250/flow7_1_250.csv'),
                         read.csv('runs_751_1000/flow7_751_1000.csv'),
                         read.csv('runs_1001_1250/flow7_1001_1250.csv'))
 
-# Maxmin
+# Maxmin pressure
 # 1-250 has different column labels, rename
 tmp0 <- read.csv('runs_1_250/maxmin_1_250.csv')
 colnames(tmp0)[2:3] <- c('max_pressure', 'min_pressure')
